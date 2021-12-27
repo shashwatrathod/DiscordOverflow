10bbfe71dcfe7ed812cbc416741df1acea186d3f
Shashwat <shashwatrathod22@gmail.com>
2021-12-27 13:01:48 -0500

    ADD: TS to dev env

---
+++ config.ts
@@ -0,0 +1,4 @@
+//Add all the env variables in this file.
+
+
+exports.discordBotToken = process.env.DISCORD_BOT_TOKEN;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
 //  main.ts is the entry point to the application
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const config = require("../config.ts");
